fbe1b6bb8d4a5c026400963c2e3248bc2f3434ac
Mito6211 <willmoore674@gmail.com>
2021-03-19 18:30:54 -0400

    API: refactor, added caching

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,40 +1,42 @@
+const FIVE_MINUTES = 5 * 60 * 1000;
+const GITHUB_URL = "https://api.github.com/users/Mito9999";
+const TYPING_URL =
+  "https://statboard.vercel.app/api/proxy/10fastfingers.com/users/get_graph_data/0/2069581";
+const TYPING_OPTIONS = {
+  method: "GET",
+  headers: {
+    authority: "10fastfingers.com",
+    "content-length": "0",
+    accept: "application/json, text/javascript, */*; q=0.01",
+    "user-agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Safari/537.36",
+    "x-requested-with": "XMLHttpRequest",
+    origin: "https://10fastfingers.com",
+  },
+  redirect: "follow",
+};
+
+import cache from "memory-cache";
 export default async function handler(_, res) {
   try {
-    const githubRes = await fetch(
-      "https://api.github.com/users/Mito9999/events"
-    );
-    const ghData = await githubRes.json();
-    const githubData = ghData
+    const values = cache.get("values");
+    if (values) {
+      res.json({ isCached: true, ...values });
+      return;
+    }
+
+    const github = fetch(GITHUB_URL + "/events").then((r) => r.json());
+    const profile = fetch(GITHUB_URL).then((r) => r.json());
+    const typing = fetch(TYPING_URL, TYPING_OPTIONS).then((r) => r.json());
+
+    const data = await Promise.all([github, profile, typing]);
+
+    const ghData = data[0]
       .filter((event) => event.type === "PushEvent")
       .slice(0, 5);
+    const { public_repos: repos } = data[1];
+    const { avg_norm, graph_data, languages_sorted } = data[2];
 
-    const ghProfileRes = await fetch("https://api.github.com/users/Mito9999");
-    const { public_repos: repos } = await ghProfileRes.json();
-
-    const headersArray = [
-      ["authority", "10fastfingers.com"],
-      ["content-length", "0"],
-      ["accept", "application/json, text/javascript, */*; q=0.01"],
-      [
-        "user-agent",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Safari/537.36",
-      ],
-      ["x-requested-with", "XMLHttpRequest"],
-      ["origin", "https://10fastfingers.com"],
-    ];
-    let myHeaders = new Headers();
-    headersArray.forEach((header) => myHeaders.append(header[0], header[1]));
-
-    const typingRes = await fetch(
-      "https://statboard.vercel.app/api/proxy/10fastfingers.com/users/get_graph_data/0/2069581",
-      {
-        method: "GET",
-        headers: myHeaders,
-        redirect: "follow",
-      }
-    );
-    const { avg_norm, graph_data, languages_sorted } = await typingRes.json();
-    const totalTypingTests = languages_sorted[0][0].anzahl;
     const last5Scores = graph_data
       .slice(graph_data.length - 5, graph_data.length)
       .map((scoresObj) => ({
@@ -43,14 +45,18 @@ export default async function handler(_, res) {
         mistakes: scoresObj.backspace_pressed,
       }))
       .reverse();
+    const totalTypingTests = languages_sorted[0][0].anzahl;
 
     const typingData = {
-      scores: last5Scores,
       average: avg_norm,
+      scores: last5Scores,
       testsTaken: Number(totalTypingTests),
     };
-    res.status(200).json({ repos, githubData, typingData });
+    const jsonData = { repos, ghData, typingData };
+    cache.put("values", jsonData, FIVE_MINUTES);
+    res.status(200).json({ isCached: false, ...jsonData });
   } catch (err) {
+    console.log(err);
     res.status(500);
   }
 }
