8917c1f5c0d47eb2b7e78372c1ff7f0c390531de
Mito6211 <willmoore674@gmail.com>
2021-03-22 16:03:22 -0400

    Add services info

--- pages/services.js
+++ pages/services.js
@@ -1,6 +1,39 @@
 import Head from "next/head";
-import { Box } from "@chakra-ui/react";
+import { Box, Text, Heading } from "@chakra-ui/react";
 import Title from "../components/Title";
+
+const servicesList = [
+  {
+    title: "Web Development",
+    price: "$25-35/hr",
+    description:
+      "I will build your web application with the technologies of your choice. I can implement your existing design, or create one from scratch. Unlimited revisions, pages, and redesigns are available.",
+  },
+  {
+    title: "Web Design",
+    price: "$25/hr",
+    description:
+      "I will create the design of your web application in Figma or Adobe XD. Available as PDF, PNG, JPG, SVG, or direct link.",
+  },
+  {
+    title: "Teaching",
+    price: "$20/hr",
+    description:
+      "I will teach or tutor you in HTML, CSS, JavaScript, or React. Will also do code reviews and help with debugging.",
+  },
+  {
+    title: "Search Engine Optimization",
+    price: "$20/hr",
+    description:
+      "I will make your website more indexible by search engines. This gives your website more potential to rank higher on search engines such as Google, and increase sales or traffic.",
+  },
+  {
+    title: "Hosting",
+    price: "",
+    description:
+      "I will host your website cheaper than cloud providers such as Amazon Web Services, Google Cloud Platform, and Azure. Pricing varies by site.",
+  },
+];
 
 export default function Services() {
   return (
@@ -12,12 +45,15 @@ export default function Services() {
 
       <main>
         <Title text="Services" />
-
-        <Box mb="40px">Service #1</Box>
-        <Box mb="40px">Service #2</Box>
-        <Box mb="40px">Service #3</Box>
-        <Box mb="40px">Service #4</Box>
-        <Box mb="40px">Service #5</Box>
+        {servicesList.map((service) => (
+          <Box key={service.title} mb="25px">
+            <Heading size="md">
+              {service.title}
+              {service.price.length > 0 && ` - ${service.price}`}
+            </Heading>
+            <Text>{service.description}</Text>
+          </Box>
+        ))}
       </main>
     </>
   );
