f9e796cd0006c19242d5cca68315fd9a32d1d83b
Mito6211 <willmoore674@gmail.com>
2021-06-01 22:55:55 -0400

    Add custom redirects

--- next.config.js
+++ next.config.js
@@ -7,4 +7,23 @@ module.exports = {
     locales: ["en-US"],
     defaultLocale: "en-US",
   },
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/Mito9999",
+        permanent: true,
+      },
+      {
+        source: "/discord",
+        destination: "https://discord.com/users/570383339811504159",
+        permanent: true,
+      },
+      {
+        source: "/email",
+        destination: "mailto:mitomandev@gmail.com",
+        permanent: true,
+      },
+    ];
+  },
 };
